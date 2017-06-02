6a785dd8b9e655a807c88eef4b96fa78e7b5c4a7
omkar langhe <omkar>
2017-06-02 21:34:56 +0530

    changes made in GET method

--- workingwithGETVar.php
+++ workingwithGETVar.php
@@ -1,13 +1,16 @@
 <?php 
 
 	 if (isset($_GET['day'])&&isset($_GET['date'])&&isset($_GET['year'])) {
+		 
 		 $day = htmlentities($_GET['day']);
 		 $date = htmlentities($_GET['date']);
 		 $year = htmlentities($_GET['year']);
 
 		if(!empty($day)&&!empty($date)&&!empty($year)) {
+			
 			echo 'It is '.$day.' '.$date.' '.$year;
-		}else{
+		
+		} else {
 
 			echo 'Fill in all fields';
 		}		 
@@ -18,6 +21,7 @@
 ?>
 
 <form action="workingwithGETVar.php" method="GET">
+	
 	Day:<br><input type="text" name="day"><br>
 	Date:<br><input type="text" name="date"><br>
 	Year:<br><input type="text" name="year"><br><br>
