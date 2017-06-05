7da4e24c31ab713cc7000d606297b4f880797871
omkar langhe <omkar>
2017-06-06 00:06:52 +0530

    uploading and restricting a file in php

---
+++ fileupload/upload.php
@@ -0,0 +1,49 @@
+<?php
+
+	@$name = $_FILES['file']['name'];
+	@$extension = strtolower(substr($name, strpos($name, '.')+ 1));
+	@$type = $_FILES['file']['type'];
+	@$size = $_FILES['file']['size'];
+	@$max_size = 100000;
+	@$tmp_name = $_FILES['file']['tmp_name'];	
+	 
+
+	 if (isset($name)) 
+	 {
+
+	 	if (!empty($name)) 
+	 	{
+
+	 		if (($extension=='jpg'||$extension=='jpeg') && $type=='image/jpeg' && $size< $max_size) {
+	 			
+	 		$location = 'uploades/';
+
+	 		if (move_uploaded_file($tmp_name,$location.$name)) 
+	 		{
+	 			echo 'Uploaded!';
+	 		} else {
+	 		
+	 			echo 'Their was an error';
+	 		}
+
+	 		} else {
+	 			
+	 			echo 'File must be jpg/jpeg and must be 2mb or less.';
+	 		}
+
+	 	 } else {
+	 		
+	 		echo 'Please choose a file.';
+	 		
+	 		}
+	 }
+	
+
+?>
+
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+
+	<input type="file" name="file"><br></br>
+	<input type="submit" name="submit">
+	
+</form>
