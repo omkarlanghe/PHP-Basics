401620dc9148fba481be95692911ae348182d2e4
omkar langhe <omkar>
2017-07-22 12:36:35 +0530

    changes made

--- loginregistration/core.inc.php
+++ loginregistration/core.inc.php
@@ -4,7 +4,7 @@ ob_start();
 session_start();
 
 $current_file = $_SERVER['SCRIPT_NAME'];
-$http_referer = $_SERVER['HTTP_REFERER']; //tell us the page we have come from..
+@$http_referer = $_SERVER['HTTP_REFERER']; //tell us the page we have come from..
 
 function loggedin() {
 
